afe2ce855f6ba8a4e2b87cbfc3b53ac307bdfdaf
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-02-19 12:26:59 +0000

    Commuted properties added to Slider puzzle component. To check if the user has one the game & to record the time between the start and finsh of each game

---
+++ src/components/SliderPuzzle.vue
@@ -0,0 +1,83 @@
+<template>
+    
+</template>
+
+<script>
+// import moment library
+import moment from "moment";
+
+// correct order of array of puzzle images
+// to be compared with user's ordered array of puzzle images
+const correctPuzzleArray = [
+    "image_part_001.jpg",
+    "image_part_002.jpg",
+    "image_part_003.jpg",
+    "image_part_004.jpg",
+    "image_part_005.jpg",
+    "image_part_006.jpg",
+    "image_part_007.jpg",
+    "image_part_008.jpg",
+    "image_part_009.jpg",
+];
+
+export default {
+    name: "SliderPuzzle",
+    props: {
+        // puzzle selected by user to be played 
+        puzzleId: {
+            type: String,
+            default: "cut-easy",
+        },
+    },
+    data(){
+        return {
+            // reative properties
+            correctPuzzleArray,
+            // shuffled array is a copy of the correct array
+            shuffledPuzzleArray: [...correctPuzzleArray].sort(
+            // randon sort 
+                () => Math.random() - 0.5
+            ),
+            // store images selected by user
+            // must be a max of two
+            indexesToSwap: [],
+            timer: undefined,
+            startDateTime: new Date(),
+            currentDateTime: new Date(),
+        };
+    },
+    computed: {
+        isWinning(){
+            // compares correct array to shuffled array
+            for (let i = 0; i < correctPuzzleArray.length; i++){
+                // if order does NOT match
+                if(correctPuzzleArray[i] !== this.shuffledPuzzleArray[i]){
+                    // games continues
+                    return false;
+                }
+                // if order DOES match
+                // User wins
+                return true; // isWinning message 
+            }
+        },
+        elapsedDiff(){
+            const currentDateTime = moment(this.currentDateTime);
+            const startDateTime = moment(this.startDateTime);
+            // time taken for completion
+            return currentDateTime.diff(startDateTime);
+            
+        },
+        elapsedTime(){
+            // ndisplay in human readable format 
+            return moment.utc(this.elapsedDiff().format('HH:mm:SS'));
+        }
+    },
+    methods: {}
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
